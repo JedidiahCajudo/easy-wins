b9df83254777dbdabe8787d95c81292032461afc
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-18 15:47:53 +0900

    if you use .file it will look for a file

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import SubtaskInput from '.SubtaskInput';
+import SubtaskInput from './SubtaskInput';
 
 function TaskInput({ addTask }) {
   const [taskTitle, setTaskTitle] = useState('');
